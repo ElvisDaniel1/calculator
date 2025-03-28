63adb4144df8991db9138207b5ffa7023ce1e556
ElvisDaniel1 <elvisdabiedeen5@gmail.com>
2025-03-28 09:19:36 -0400

    Add working buttons with working functions

--- calculator.js
+++ calculator.js
@@ -2,74 +2,46 @@
 let numA = 0;
 let numB = 0;
 let operator = '';
+let answer = '';
+
+capture();
 
 //Send to appropriate function to operate on
 function operate(a, b, operator) {
     if (operator == '+'){
-        console.log(add(a, b));
+        return(a + b);
     }
     else if (operator == '-'){
-        minus(a, b);
+        return(a - b);
     }
     else if (operator == '*'){
-        multiply(a, b);
+        return (a * b);
     }
     else
-        divide(a, b); 
+        return (a / b); 
 }
 
-
-// Math functions
-function add(a, b){''
-    return a + b;
-}
-
-function minus(a, b){
-    return a - b;
-}
-
-function multiply(a, b){
-    return a * b;
-}
-
-function divide(a, b){
-    return a / b;
-}
-
-//a = prompt('Please enter a number:  ');
-//operator = prompt('Please enter an operator: ');
-//b = prompt('Please enter a second number: ');
-
-//operate(a, b, operator);
 
 // Capturing buttons
-let strNumber = '';
-document.addEventListener("DOMContentLoaded", () => {
+function capture() {
+    let strNumber = '';
+    document.addEventListener("DOMContentLoaded", () => {
     const numBtn = document.querySelectorAll(".btn-num");
     numBtn.forEach((button) => {
         button.addEventListener("click", () => {
-            //console.log(button.textContent) //Print each button press to console
-             
+                 
             let input = button.textContent;
             strNumber = strNumber + input;
             //inputProcessing(input); run command when u get an equal sign
-            if (input === 'C') {
-                const element = document.getElementById("output");
-                while (element.firstChild) {
-                    element.removeChild(element.firstChild);
-                }
-            }
+            if (input === 'C') 
+                clearScreen();
                 
-
             if (input === '=')
                 evaluate(strNumber);
 
             //let txtCont = input; 
-            if (input != 'C') {
-                const node = document.createTextNode(input);
-            
-                const para = document.createElement("p");
-                para.appendChild(node);
+            if (input != 'C' && input != '=') {
+                const para = drawToScreen(input);
 
                 const outElement = document.getElementById("output");
                 outElement.appendChild(para);
@@ -79,6 +51,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+}
+
+function drawToScreen(nodeIn){
+    const node = document.createTextNode(nodeIn);
+
+    const para = document.createElement("p");
+    para.appendChild(node);
+
+    return para;
+}
+
+function clearScreen(){
+    const element = document.getElementById("output");
+                while (element.firstChild) {
+                    element.removeChild(element.firstChild);
+                }
+}
 
 // Capture user input at '=' and handle string
 function evaluate(strNumber){
@@ -92,14 +82,21 @@ function evaluate(strNumber){
     // How do we determine what to split on???
     let operands = strNumber.split(operator);
 
-    let numA = operands[0];
+    let numA = Number(operands[0]);
     let numB = operands[1];
     
     numB = numB.replace('=', '');
+    numB = Number(numB);
 
+    answer = operate(numA, numB, operator);
 
-    //console.log(numA);
-    //console.log(numB);
-    //console.log(strNumber);
+    // Output calculated answer
+    clearScreen();
+    const target = document.getElementById("output");
+    
+    const para = drawToScreen(answer);
+
+    
+    target.appendChild(para);
 }
 
